89b814adbf02c96d09ca93797bcf502bd20cce38
mwerlen <maxime@werlen.fr>
2020-04-18 15:23:18 +0200

    Automount disk

--- backup_server_partial_media_disk_on_mini_external_drive.sh
+++ backup_server_partial_media_disk_on_mini_external_drive.sh
@@ -1,4 +1,25 @@
-#!/bin/sh
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+# Make sure only root can run our script
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+if [[ -z "${STY:-}" ]]; then
+    echo "You are not running this backup in a GNU screen."
+    echo "You perhaps forget to run with sudo -E to keep your env variables"
+    read -p "Press [Enter] key to start backup anyway..."
+fi
+
+mkdir -p /mnt/backup
+cryptsetup luksOpen /dev/disk/by-uuid/3919452e-9ba5-496e-9c96-e91d1672eb5c backup
+mount /dev/mapper/backup /mnt/backup
+
+echo "Disk mounted"
+echo "Starting backup. You will receive a mail when backup is done."
 
 START=$(date +%s)
 rsync-no-vanished \
@@ -20,5 +41,10 @@ FINISH=$(date +%s)
 echo "Backup on external drive total time: $(( ($FINISH-$START) / 60 )) minutes, $(( ($FINISH-$START) % 60 )) seconds"
 echo "Backup on external drive total time: $(( ($FINISH-$START) / 60 )) minutes, $(( ($FINISH-$START) % 60 )) seconds" | mail -s "Backup on external drive" root@server.werlen.fr
 
-rm /mnt/backup/XX-Backup*
+rm -f /mnt/backup/XX-Backup*
 touch /mnt/backup/"XX-Backup from $(date '+%A, %d %B %Y, %T')"
+
+umount /mnt/backup
+cryptsetup luksClose backup
+
+echo "Done !"
